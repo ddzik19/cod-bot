740b03b944ac86777e480eb8e95c1e61e6a878eb
ddzik19 <ddzik09@gmail.com>
2022-01-27 17:18:57 +0000

    BIG UPDATE (not really)
    -Fixed commands not working
    -Use new prefix for commands !wz with separator of full-stop ( . )
    -Fixed embeds
    -Added Bot icon (don't sue me)
    -installed discord.js and dotenv

---
+++ bot.js
@@ -0,0 +1,65 @@
+/** 
+    Author: Damian Dzik
+    Date: 22/01/2022
+    Updated: 12/01/2022
+**/
+
+// basic setup stuff ---------
+// allowing us to get the token from the .env file
+require('dotenv').config();
+const Discord = require('discord.js');
+const {
+    Client,
+    Intents
+} = require('discord.js')
+const client = new Client({
+    restTimeOffset: 0,
+    intents: [Intents.FLAGS.GUILDS,
+        Intents.FLAGS.GUILD_MESSAGES,
+        Intents.FLAGS.GUILD_MESSAGE_REACTIONS,
+    ]
+})
+//----------------------------
+
+// creating a file sync reader to read files
+const fs = require('fs')
+
+// creating a new collection of commands
+client.commands = new Discord.Collection();
+
+// getting all command files 
+const commandFiles = fs.readdirSync('commands').filter(file => file.endsWith('.js'));
+
+// looping through files in commands and adding the commands into a collection
+for (const file of commandFiles) {
+    const command = require(`./commands/${file}`)
+    client.commands.set(command.name, command);
+}
+
+// when bot is logged in display message
+client.on('ready', () => {
+    console.log(`Logged in as ${client.user.tag}!`)
+})
+
+// commands
+// the prefix of each command
+const prefix = "!wz"
+client.on("messageCreate", msg => {
+    const args = msg.content.trim().split('.')
+    const command = args[0].toLowerCase();
+
+    if (msg.author.bot || !msg.content.startsWith(prefix)) return
+
+    try {
+        // listing all the viable cold war weapon builds
+        if (command === "!wz") {
+            client.commands.get(args[1].toLowerCase()).execute(msg);
+        }
+    } catch (error) {
+        console.log(error)
+    }
+});
+
+(async () => {
+    client.login(process.env.TOKEN)
+})();
